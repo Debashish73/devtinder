7a9bf7753432d3c9c5bea0217f1f820d5eebeafd
Debashish73 <debashishjena119@gmail.com>
2025-09-06 12:30:56 +0530

    exploring get,post,delete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("HII");
+const express=require('express');
+const app=express();
+app.get('/user',(req,res)=>{
+    res.send({Firstname:"Debashish",Lastname:"Jena"})
+})
+
+app.post('/user',(eq,res)=>{
+    res.send("Data Saved Successfully ")
+})
+
+app.delete('/user',(req,res)=>{
+    res.send("Delete Done")
+})
+
+app.listen('3000');
+console.log("Run well");
